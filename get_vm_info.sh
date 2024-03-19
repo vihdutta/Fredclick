38c54a486813939882e4697927390216e06b6d80
vihdutta <vihdutta@gmail.com>
2024-03-19 17:30:20 -0400

    implemented automated deployment for 2 distributed servers useast

--- get_vm_info.sh
+++ get_vm_info.sh
@@ -1,2 +1,2 @@
 curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | \
-python -c 'import json, sys; data = json.load(sys.stdin); print(json.dumps({"name": data["compute"]["name"], "location": data["compute"]["location"]}));' >> vm_info.json
+python3 -c 'import json, sys; data = json.load(sys.stdin); print(json.dumps({"name": data["compute"]["name"], "location": data["compute"]["location"]}));' >> vm_info.json
